c5ac376339fd04f5f18238d4ca64084e3cfe65a9
Albert-SA <albert_sa@mail.ru>
2024-01-05 20:46:33 +0500

    edit -showMoreStyles('#styles .row')

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', ():void => {
   mask('[name = "phone"]');
   checkTextInputs('[name = "name"]');
   checkTextInputs('[name = "message"]');
-  showMoreStyles('.button-styles', '.styles-2');
+  showMoreStyles('.button-styles', '#styles .row');
   calc({
     size: '#size',
     material: '#material',
