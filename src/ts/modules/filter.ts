4bbe9a4ddcfb8dbd7516cb36b966a92da9d6ff63
Albert-SA <albert_sa@mail.ru>
2024-01-06 10:47:05 +0500

    08-create-clickBtnFilter

--- src/ts/modules/filter.ts
+++ src/ts/modules/filter.ts
@@ -2,20 +2,9 @@ const filter = () => {
     const menu: HTMLElement | null = document.querySelector('.portfolio-menu');
     if (!menu) return;
     const itemsMenu: NodeListOf<HTMLElement> = menu.querySelectorAll('li');
-    const btnAll: HTMLElement | null = menu.querySelector('.all');
-    const btnLovers: HTMLElement | null = menu.querySelector('.lovers');
-    const btnChef: HTMLElement | null = menu.querySelector('.chef');
-    const btnGirl: HTMLElement | null = menu.querySelector('.girl');
-    const btnGuy: HTMLElement | null = menu.querySelector('.guy');
-    const btnGrandmother: HTMLElement | null = menu.querySelector('.grandmother');
-    const btnGrandad: HTMLElement | null = menu.querySelector('.granddad');
     const wrapper: HTMLElement | null = document.querySelector('.portfolio-wrapper');
     if (!wrapper) return;
     const markAll: NodeListOf<HTMLElement> = wrapper.querySelectorAll('.all');
-    const markLovers: NodeListOf<HTMLElement> = wrapper.querySelectorAll('.lovers');
-    const markChef: NodeListOf<HTMLElement> = wrapper.querySelectorAll('.chef');
-    const markGirl: NodeListOf<HTMLElement> = wrapper.querySelectorAll('.girl');
-    const markGuy: NodeListOf<HTMLElement> = wrapper.querySelectorAll('.guy');
     const noMark: HTMLElement | null = document.querySelector('.portfolio-no');
 
     const typeFilter = (markType: NodeListOf<HTMLElement>) => {
@@ -39,34 +28,23 @@ const filter = () => {
         }
     };
 
-    btnAll?.addEventListener('click', () => {
-        typeFilter(markAll);
-    });
+    const clickBtnFilter = (ourSelector: string) => {
+        const btn: HTMLElement | null = menu.querySelector(ourSelector);
+        const mark: NodeListOf<HTMLElement> = wrapper.querySelectorAll(ourSelector);
+        
+        btn?.addEventListener('click', () => {
+            typeFilter(mark);
+        });
+    };
 
-    btnLovers?.addEventListener('click', () => {
-        typeFilter(markLovers);
-    });
-
-    btnChef?.addEventListener('click', () => {
-        typeFilter(markChef);
-    });
-
-    btnGuy?.addEventListener('click', () => {
-        typeFilter(markGuy);
-    });
-
-    btnGirl?.addEventListener('click', () => {
-        typeFilter(markGirl);
-    });
-
-    btnGrandmother?.addEventListener('click', () => {
-        typeFilter();
-    });
-
-    btnGrandad?.addEventListener('click', () => {
-        typeFilter();
-    });
-
+    clickBtnFilter('.all');
+    clickBtnFilter('.lovers');
+    clickBtnFilter('.chef');
+    clickBtnFilter('.girl');
+    clickBtnFilter('.guy');
+    clickBtnFilter('.grandmother');
+    clickBtnFilter('.granddad');
+   
     menu.addEventListener('click', (e) => {
         const target: any = e.target;
 
